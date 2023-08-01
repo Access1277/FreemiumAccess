49a7b259d354c28fc0fa09c560c3ea4ba3d651f4
Access1277 <108107724+Access1277@users.noreply.github.com>
2023-08-01 18:10:04 +0800

    Create proxy.sh

---
+++ proxy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Remote server details
+remote_server="123.456.789.123"  # Replace with the IP address or hostname of the remote server
+remote_port="22"                # Replace with the SSH port if it's not the default (port 22)
+
+# Local proxy details
+local_port="8080"               # Replace with your preferred local port for the proxy (e.g., 8888)
+
+# Start the SSH tunnel for the proxy
+ssh -N -D "$local_port" "$remote_server" -p "$remote_port"
