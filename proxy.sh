1bb6184a95499039622cd9dd3a03a005bbf010d5
Access1277 <108107724+Access1277@users.noreply.github.com>
2023-08-01 18:17:56 +0800

    Update proxy.sh

--- proxy.sh
+++ proxy.sh
@@ -2,10 +2,24 @@
 
 # Remote server details
 remote_server="159.223.92.20"  # Replace with the IP address or hostname of the remote server
-remote_port="8080"                # Replace with the SSH port if it's not the default (port 22)
+remote_port="22"                # Replace with the SSH port if it's not the default (port 22)
 
 # Local proxy details
-local_port="8000"               # Replace with your preferred local port for the proxy (e.g., 8888)
+local_port="8080"               # Replace with your preferred local port for the proxy (e.g., 8888)
 
-# Start the SSH tunnel for the proxy
-ssh -N -D "$local_port" "$remote_server" -p "$remote_port"
+# Function to check if SSH is available
+check_ssh() {
+  if ! command -v ssh &>/dev/null; then
+    echo "SSH is not installed. Please install OpenSSH to use the proxy."
+    exit 1
+  fi
+}
+
+# Function to start the proxy
+start_proxy() {
+  check_ssh
+  echo "Starting the proxy..."
+  ssh -N -D "$local_port" "$remote_server" -p "$remote_port"
+}
+
+start_proxy
