ebec3a9bb466dcf2785ed07a359b25e0d3f0cf6b
Access1277 <108107724+Access1277@users.noreply.github.com>
2023-08-13 15:23:51 +0800

    Create badvpn.sh

---
+++ badvpn.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Created by Mei 2020
+# Modified by IDTunnel
+
+# install badvpn
+cd
+wget -O /usr/bin/badvpn-udpgw "https://github.com/idtunnel/UDPGW-SSH/raw/master/badvpn-udpgw"
+if [ "$OS" == "x86_64" ]; then
+  wget -O /usr/bin/badvpn-udpgw "https://github.com/idtunnel/UDPGW-SSH/raw/master/badvpn-udpgw64"
+fi
+
+wget -O /bin/badvpn-udpgw "https://github.com/idtunnel/UDPGW-SSH/raw/master/badvpn-udpgw"
+if [ "$OS" == "x86_64" ]; then
+  wget -O /bin/badvpn-udpgw "https://github.com/idtunnel/UDPGW-SSH/raw/master/badvpn-udpgw64"
+fi
+
+#port BadVPN 7300
+sed -i '$ i\screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300' /etc/rc.local
+sed -i '$ i\screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300' /etc/rc.d/rc.local
+
+#port BadVPN 7300
+sed -i '$ i\screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7100' /etc/rc.local
+sed -i '$ i\screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7100' /etc/rc.d/rc.local
+
+#port BadVPN 7300
+sed -i '$ i\screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200' /etc/rc.local
+sed -i '$ i\screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200' /etc/rc.d/rc.local
+
+#permission
+chmod +x /usr/bin/badvpn-udpgw
+chomod +x /etc/rc.d/rc.local
+
+#badvpn 7300
+screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300
