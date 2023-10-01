70baedcd65ef4f18e55b404a5421da61b43cc66b
Access1277 <108107724+Access1277@users.noreply.github.com>
2023-10-01 08:15:19 +0800

    Update gtmnoload.sh

--- gtmnoload.sh
+++ gtmnoload.sh
@@ -1,11 +1,7 @@
 NS='sdns.art1.bagito.tech'
 A='art1.bagito.tech'
-NS1='aotns-2pen9.givpn.online'
-A1='aot-6cw7d.givpn.online'
-NS2='ns.gtmsdns.bagito.tech'
-A2='gtmsdns.bagito.tech'
-NS3='jrc.ws.privatessh.tech'
-A3='kjrc.ws.privatessh.tech'
+NS1='sdns.lantin12.elcavlaw.com'
+A1='lantin12.elcavlaw.com'
 LOOP_DELAY=5
 
 declare -a HOSTS=('112.198.115.44' '112.198.115.36' '124.6.181.36' '124.6.181.20')
@@ -39,7 +35,7 @@ endscript() {
 trap endscript 2 15
 check(){
  for ((i=0; i<"${#HOSTS[*]}"; i++)); do
-  for R in "${NS}" "${A}" "${NS1}" "${A1}" "${NS2}" "${A2}" "${NS3}" "${A3}"; do
+  for R in "${NS}" "${A}" "${NS1}" "${A1}"; do
    T="${HOSTS[$i]}"
      $(timeout -k .3 .3 ${_DIG} @${T} ${R})  && M=31 || M=32;
    echo -e "\e[1;${M}m\$ R:${R} D:${T}\e[0m"
