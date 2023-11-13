a80013128a008ca18547d7f3c7da81c909ed539c
Access1277 <108107724+Access1277@users.noreply.github.com>
2023-11-13 21:30:07 +0800

    Update gtmnoload.sh

--- gtmnoload.sh
+++ gtmnoload.sh
@@ -1,68 +1,61 @@
-#!/bin/bash
-
-# Improved DNSTT Keep-Alive Script
-# Copyright © ELCAVLAW
-# Recoded by: LANTIN
-
-# Your DNSTT Nameservers
-NAMESERVERS=('sdns.myudp.elcavlaw.com' 'team-mamawers.elcavlaw.com')
-# Your Domain `A` Record
-A_RECORD='myudp.elcavlaw.com'
-A_RECORD='mamawers.elcavlaw.com'
-
-# Add your DNS here
-HOSTS=('124.6.181.12' '112.198.115.44' '112.198.115.36')
-
-# Loop delay in seconds (positive integer only)
+NS='sdns.myudp.elcavlaw.com'
+NS1='team-mamawers.elcavlaw.com
 LOOP_DELAY=5
-if ! [[ "$LOOP_DELAY" =~ ^[1-9][0-9]*$ ]]; then
-    echo "Invalid LOOP_DELAY. Must be a positive integer."
-    exit 1
-fi
 
-# Check and set the dig command
-DIG_EXEC=${DIG_EXEC:-"DEFAULT"} # Use DEFAULT if not set
-CUSTOM_DIG='/data/data/com.termux/files/home/go/bin/fastdig'
-DIG_CMD=""
+declare -a HOSTS=('124.6.181.12' '112.198.115.36' '112.198.115.44')
+DIG_EXEC="DEFAULT"
 
-# Determine the dig command based on user choice
+CUSTOM_DIG=/data/data/com.termux/files/home/go/bin/fastdig
+
+######################################
+######################################
+######################################
+######################################
+######################################
+VER=0.1
 case "${DIG_EXEC}" in
-    DEFAULT|D) DIG_CMD="$(command -v dig)" ;;
-    CUSTOM|C) DIG_CMD="${CUSTOM_DIG}" ;;
-    *) echo "Invalid DIG_EXEC value. Please choose DEFAULT or CUSTOM."; exit 1 ;;
+ DEFAULT|D)
+ _DIG="$(command -v dig)"
+ ;;
+ CUSTOM|C)
+ _DIG="${CUSTOM_DIG}"
+ ;;
 esac
-
-# Verify that the dig command is available
-[ -z "${DIG_CMD}" ] && { echo "Dig command not found. Please install dnsutils or set the correct path in CUSTOM_DIG."; exit 1; }
-
-# Function to check the DNS
-check_dns() {
-    for host in "${HOSTS[@]}"; do
-        for ns in "${NAMESERVERS[@]}" "${A_RECORD}"; do
-            if ! timeout -k 3 3 "${DIG_CMD}" "@${host}" "${ns}" &> /dev/null; then
-                echo "R:${ns} D:${host} - failure"
-            else
-                echo "R:${ns} D:${host} - success"
-            fi
-        done
-    done
-}
-
-# Main execution
-echo "DNSTT Keep-Alive script Modified by: LANTIN"
-echo "DNS List: ${HOSTS[*]}"
-echo "CTRL + C to close the script"
-
-# Loop or single check based on argument
-if [ "${1,,}" == "loop" ] || [ "${1,,}" == "l" ]; then
-    echo "Script loop: ${LOOP_DELAY} seconds"
-    while true; do
-        check_dns
-        echo 'LANTIN NOHANIH'
-        sleep "${LOOP_DELAY}"
-    done
-else
-    check_dns
+if [ ! $(command -v ${_DIG}) ]; then
+ printf "%b" "Dig command failed to run, " \
+ "please install dig(dnsutils) or check " \
+ "\$DIG_EXEC & \$CUSTOM_DIG variable inside $( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )/$(basename "$0") file.\n" && exit 1
 fi
-
+endscript() {
+ unset NS A LOOP_DELAY HOSTS _DIG DIG_EXEC CUSTOM_DIG T R M
+ exit 1
+}
+trap endscript 2 15
+check(){
+ for ((i=0; i<"${#HOSTS[*]}"; i++)); do
+  for R in "${NS}" "${NS1}"; do
+   T="${HOSTS[$i]}"
+     $(timeout -k .3 .3 ${_DIG} @${T} ${R})  && M=31 || M=32;
+   echo -e "\e[1;${M}m\$ R:${R} D:${T}\e[0m"
+   unset T R M
+  done
+ done
+}
+echo "DNSTT Keep-Alive script <Discord @civ3>"
+echo -e "DNS List: [\e[1;34m${HOSTS[*]}\e[0m]"
+echo "CTRL + C to close script"
+ "${LOOP_DELAY}" -eq 1  && let "LOOP_DELAY++";
+case "${@}" in
+ loop|l)
+ echo "Script loop: ${LOOP_DELAY} seconds"
+ while true; do
+  check
+  echo '.--. .-.. . .- ... .     .-- .- .. -'
+  sleep ${LOOP_DELAY}
+ done
+ ;;
+ *)
+ check
+ ;;
+esac
 exit 0
