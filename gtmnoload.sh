a562eb9108cd88e10fc399ed561f67f07c09f494
Access1277 <108107724+Access1277@users.noreply.github.com>
2023-11-26 19:37:52 +0800

    Update gtmnoload.sh

--- gtmnoload.sh
+++ gtmnoload.sh
@@ -1,22 +1,19 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
 NS='sdns.myudp.elcavlaw.com'
-A='myudp.elcavlaw.com'
 NS1='sdns.myudph.elcavlaw.com'
-A1='myudph.elcavlaw.com'
 NS2='sdns.myudp1.elcavlaw.com'
-A2='myudp1.elcavlaw.com'
 NS3='ns-sgfree.elcavlaw.com'
-A3='sgfree.elcavlaw.com'
+
 
 LOOP_DELAY=0
 
-declare -a HOSTS=('124.6.181.12' '124.6.181.36' '124.6.181.20')
+declare -a HOSTS=('124.6.181.12' '124.6.181.36')
 
 DIG_EXEC="DEFAULT"
 CUSTOM_DIG="/data/data/com.termux/files/home/go/bin/fastdig"
 
-_VER=0.7
+_VER=0.1
 
 case "${DIG_EXEC}" in
   DEFAULT|D)
@@ -32,31 +29,31 @@ if ! command -v "${_DIG}" &>/dev/null; then
   exit 1
 fi
 
+get_public_ip() {
+  echo "$(curl -s ipinfo.io/ip)"
+}
+
 endscript() {
-  unset NS A NS1 A1 NS2 A2 NS3 A3 LOOP_DELAY HOSTS _DIG DIG_EXEC CUSTOM_DIG T R M
+  unset NS NS1 NS2 NS3 LOOP_DELAY HOSTS _DIG DIG_EXEC CUSTOM_DIG T R M
   exit 1
 }
 
 trap endscript 2 15
 
 check() {
-  echo "DNS Query Results:"
+  local PUBLIC_IP
+  PUBLIC_IP=$(get_public_ip)
+  echo "Public IP Address: ${PUBLIC_IP}"
 
   for T in "${HOSTS[@]}"; do
     for R in "${A}" "${NS}" "${A1}" "${NS1}" "${A2}" "${NS2}" "${A3}" "${NS3}"; do
-      IP="$(timeout -k 3 3 "${_DIG}" +short "@${T}" "${R}" | tail -n 1)"
-      if [ -n "${IP}" ]; then
-        M=32
-      else
-        M=31
-      fi
-
-      echo -e "\e[1;${M}m\$ R:${R} D:${T} IP:${IP}\e[0m"
+      (timeout -k 3 3 "${_DIG}" "@${T}" "${R}") && M=32 || M=31
+      echo -e "\e[1;${M}m\$ R:${R} D:${T}\e[0m"
     done
   done
 }
 
-echo "DNSTT Keep-Alive script with Deep DNS Query <Lantin Nohanih> (v${_VER})"
+echo "DNSTT Keep-Alive script with IP Hunter <Lantin Nohanih> (v${_VER})"
 echo -e "DNS List: [\e[1;34m${HOSTS[*]}\e[0m]"
 echo "CTRL + C to close script"
 
